a2671563c4a5768e1db2acdab82d31c8ab1013a0
Tyler Conklin <Tdeec23@gmail.com>
2017-09-18 12:03:29 -0400

    Fixed File version error

--- navigation.js
+++ navigation.js
@@ -1,14 +1,4 @@
 $(document).ready(function()
 {
-    $(".dropdown").mouseenter(function()
-    {
-        $(this).children().fadeIn('fast');
-        $(this).children().animate({fontSize: 50});
-    });
-    
-    $(".dropdown").mouseleave(function()
-    {
-        $(".dropdown #list").fadeOut('fast');
-        $(".dropdown #title, #list").animate({fontSize: 19});
-    });
+    $("#header-placeholder").load("_header.html");
 });
